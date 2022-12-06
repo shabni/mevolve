993d922a4b73400e7f56242bfbb35c2c63c910ee
shawez123 <100187253+shawez123@users.noreply.github.com>
2022-12-06 20:04:39 +0500

    lannding page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactUsComponent,
+    PricingComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
