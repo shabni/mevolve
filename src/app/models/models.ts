7c3460ca381b1c12e8b452dd9bb517534123027c
Farid ullah <faridullah996@gmail.com>
2022-12-11 09:52:16 +0500

    pricing section mobile responsiveness

--- src/app/models/models.ts
+++ src/app/models/models.ts
@@ -2,3 +2,9 @@ export interface AppRule {
 	image: string;
 	title: string;
 }
+
+export interface PricePlan {
+	price: number;
+	type: 'Free' | 'Per Month' | 'Per Year';
+	duration: 'For 30 days' | 'Monthly Billing' | 'Yearly Billing';
+}
--- src/app/pricing/pricing.component.ts
+++ src/app/pricing/pricing.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { PricePlan } from '../models/models';
+
 
 @Component({
   selector: 'app-pricing',
   templateUrl: './pricing.component.html',
   styleUrls: ['./pricing.component.scss']
 })
-export class PricingComponent implements OnInit {
+export class PricingComponent {
+	pricing: PricePlan[] = [];
+	offers: string[] = [];
+	
+	constructor() {
+		this.pricing = [
+			{ price: 0, type: 'Free', duration: 'For 30 days' },
+			{ price: 2.99, type: 'Per Month', duration: 'Monthly Billing' },
+			{ price: 3.99, type: 'Per Year', duration: 'Yearly Billing' },
+		];
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+		this.offers = [
+			"Add Todos, Habits, Journals and Notes in a single app.",
+			"Four types of Habit Tracking.",
+			"Track Todos, Habits, Journals and Notes with daily values.",
+			"Update your Mood and Pictures in Journal and Notes section.",
+			"Customize the app color.",
+			"Notification and Reminders.",
+			"Lock Pin"
+		];
+	}
 }
