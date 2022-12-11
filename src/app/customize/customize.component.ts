89bb0f6746d53c9569d1c96f9b00f18a0e0bacf0
Farid ullah <faridullah996@gmail.com>
2022-12-11 08:07:19 +0500

    customize section responsiveness

--- src/app/customize/customize.component.ts
+++ src/app/customize/customize.component.ts
@@ -1,34 +1,28 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+
 
 @Component({
   selector: 'customize-view',
   templateUrl: './customize.component.html',
   styleUrls: ['./customize.component.scss']
 })
-export class CustomizeViewComponent implements OnInit, OnChanges {
+export class CustomizeViewComponent implements OnChanges {
+	@Input() imageDirection: 'left' | 'right' = 'left';
+	@Input() imageUrl: string;
+	
+	isChecked = true;
+	flexDirection: string = 'row';
 
-  @Input() imageDirection :'left' |'right' ='left'
-  @Input() imageUrl: string
-  isChecked = true
+	constructor() { }
 
-  flexDirection: string = 'row'
-
-  constructor() { }
-
-  
-
-  ngOnChanges(changes: SimpleChanges){
-
-    if (this.imageDirection === 'left') {
-      this.flexDirection = 'row'
-    }  else {
-      this.flexDirection = 'row-reverse'
-    } 
-  }
-
-  ngOnInit(): void {
-
-    console.log("++--->>")
-  }
-
+	ngOnChanges(changes: SimpleChanges){
+		if (this.imageDirection === 'left') 
+		{
+			this.flexDirection = 'row';
+		}  
+		else 
+		{
+			this.flexDirection = 'row-reverse';
+		} 
+	}
 }
